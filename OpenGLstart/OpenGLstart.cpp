35bc20b990b72e9c6abe923a622d18cd0f0131ad
Nikita <nekit.im@yandex.ru>
2023-10-05 12:44:48 +0300

    start

--- OpenGLstart/OpenGLstart.cpp
+++ OpenGLstart/OpenGLstart.cpp
@@ -66,88 +66,27 @@ void drawSmth(GLuint stm, GLuint shader_programme) {
 	{0.0f, 0.0f, 1.0f},
 	{1.0f, 1.0f, 0.0f}
 	};
-
-	GLuint indices[] = { 0, 1, 2, 2, 3, 0 };
-
-	//GLuint elementbuffer;
-	//glGenBuffers(1, &elementbuffer); // Генерация одного объекта буфера вершин
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer); // Привязка элементного буфера
-	////Загрузка индексов в используемый элементный буфер
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);//После этого для вывода объекта вместо стандартной функции glDrawArrays следует использовать функцию glDrawElements, напримерследующим образом :
-
-
-
-
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(points), points,
-	//	GL_STATIC_DRAW);
+	//GLuint indices[] = { 0, 1, 2 };
+	GLuint indices[] = { 0, 1, 2, 2, 3, 0 }; // обход вершин
 
 	glm::mat4 transformMatrix = glm::mat4(1.0f);
-	//glm::vec3 position = { 0.3,0,0 };
-	//transformMatrix = glm::translate(transformMatrix, position);
-
-	//float rotation = 0.3;
-	//transformMatrix = glm::rotate(transformMatrix, rotation, glm::vec3(0.0,	0.0, 1.0));
-
-	//glm::vec3 scale = { 0.3,0.3,0.3 };
-	//transformMatrix = glm::scale(transformMatrix, scale);
-
-	//glUniformMatrix4fv(stm, 1, GL_FALSE, &transformMatrix[0][0]);
-
-
-
-	/*for (int i = 0; i < n * 3; i++)
-		points[i] *= size;
-	for (int i = 0; i < n * 3; i += 3)
-		points[i] += startx;
-	for (int i = 1; i < n * 3; i += 3)
-		points[i] += starty;*/
-
-	//GLuint coords_vbo = 0;
-	//glGenBuffers(1, &coords_vbo);
-	//glBindBuffer(GL_ARRAY_BUFFER, coords_vbo);
-	//glBufferData(GL_ARRAY_BUFFER, 3 * 3 * sizeof(float), points,
-	//	GL_STATIC_DRAW);
-	//GLuint colors_vbo = 0;
-	//glGenBuffers(1, &colors_vbo);
-	//glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-	//glBufferData(GL_ARRAY_BUFFER, 3 * 3 * sizeof(float), colors,
-	//	GL_STATIC_DRAW);
-	//GLuint vao = 0;
-	//glGenVertexArrays(1, &vao);
-	//glBindVertexArray(vao);
-	//glBindBuffer(GL_ARRAY_BUFFER, coords_vbo);
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-	//glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-	//glEnableVertexAttribArray(0);
-	//glEnableVertexAttribArray(1);
-	//glBindVertexArray(vao);
-	//glDrawArrays(GL_TRIANGLES, 0, );
-	//glBindVertexArray(vao);
-	//glDeleteVertexArrays(1, &vao);
-	//glBindBuffer(GL_ARRAY_BUFFER, coords_vbo);
-	//glDeleteBuffers(1, &coords_vbo);
-	//glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-	//glDeleteBuffers(1, &colors_vbo);
-
 	
-	//	GLuint stm = glGetUniformLocation(shader_programme, "transform");
 		glUniformMatrix4fv(stm, 1, GL_FALSE, &transformMatrix[0][0]);
 		GLuint coords_vbo = 0;
 		glGenBuffers(1, &coords_vbo);
 		glBindBuffer(GL_ARRAY_BUFFER, coords_vbo);
-		glBufferData(GL_ARRAY_BUFFER, 3 * 3 * sizeof(GLfloat), points,
+		glBufferData(GL_ARRAY_BUFFER, sizeof(points), points,
 			GL_STATIC_DRAW);
 		GLuint colors_vbo = 0;
 		glGenBuffers(1, &colors_vbo);
 		glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-		glBufferData(GL_ARRAY_BUFFER, 3 * 3 * sizeof(GLfloat),
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colors),
 			colors, GL_STATIC_DRAW);
 		GLuint elementbuffer;
 		glGenBuffers(1, &elementbuffer);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-		int v = 9;
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 9 * sizeof(GLuint), indices, GL_STATIC_DRAW);
+		
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 		GLuint vao = 0;
 		glGenVertexArrays(1, &vao);
 		glBindVertexArray(vao);
