f3745b2f5f207716a5df1ba86a5c1438b9530718
Nikita <nekit.im@yandex.ru>
2023-11-09 22:00:28 +0300

    work_packege_lr7

---
+++ src/main/java/org/example/models/TexturedModel.java
@@ -0,0 +1,21 @@
+package org.example.models;
+
+import org.example.texture.ModelTexture;
+
+public class TexturedModel {
+    private RawModel model;
+    private ModelTexture texture;
+
+    public TexturedModel(RawModel model, ModelTexture texture){
+        this.model=model;
+        this.texture=texture;
+    }
+
+    public RawModel getModel() {
+        return model;
+    }
+
+    public ModelTexture getTexture() {
+        return texture;
+    }
+}
---
+++ src/main/java/org/example/texture/ModelTexture.java
@@ -0,0 +1,13 @@
+package org.example.texture;
+
+public class ModelTexture {
+    private  int textureId;
+
+    public ModelTexture(int textureId){
+        this.textureId=textureId;
+    }
+
+    public int getId() {
+        return textureId;
+    }
+}
